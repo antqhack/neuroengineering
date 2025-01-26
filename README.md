082551976435c8a9f12a6c63807a0fc467f9072c
Marquis Hackett <marquishackett@MacBookPro.lan>
2025-01-25 21:50:26 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# neuroengineering
-Telekinesis and other fun tricks
+# brainwaves
+
+Analyzing my own brain waves. 
+
+(brainwaves)[https://mne.tools/stable/auto_tutorials/intro/10_overview.html]
+
---
+++ brainwaves.py
@@ -0,0 +1,18 @@
+# Authors: The MNE-Python contributors.
+# License: BSD-3-Clause
+# Copyright the MNE-Python contributors.
+
+import numpy as np
+import mne
+
+sample_data_folder = mne.datasets.sample.data_path()
+sample_data_raw_file = (
+    sample_data_folder / "MEG" / "sample" / "sample_audvis_filt-0-40_raw.fif"
+)
+raw = mne.io.read_raw_fif(sample_data_raw_file)
+
+print(raw)
+print(raw.info)
+
+raw.compute_psd(fmax=50).plot(picks="data", exclude="bads", amplitude=False)
+raw.plot(duration=5, n_channels=30)
